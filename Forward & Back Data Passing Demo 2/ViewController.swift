0d1f3873b95208bc700ad97c3d219dfb6565faab
panditRachana <rachupandit93@gmail.com>
2022-11-04 14:48:54 +0530

    Initial Commit

---
+++ Forward & Back Data Passing Demo 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Forward & Back Data Passing Demo 2
+//
+//  Created by Rachana Pandit on 04/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
